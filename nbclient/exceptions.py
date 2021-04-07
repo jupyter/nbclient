ca708664a879cc91898529b77034ff48888df6fb
Ondřej Čertík <ondrej@certik.us>
2021-04-07 09:36:12 -0600

    Update nbclient/exceptions.py
    Co-authored-by: David Brochart <david.brochart@gmail.com>

--- nbclient/exceptions.py
+++ nbclient/exceptions.py
@@ -88,10 +88,7 @@ class CellExecutionError(CellControlSignal):
         """Instantiate from a code cell object and a message contents
         (message is either execute_reply or error)
         """
-        if msg.get('traceback', []):
-            tb = '\n'.join(msg.get('traceback', []))
-        else:
-            tb = ''
+        tb = '\n'.join(msg.get('traceback', []) or [])
         return cls(
             exec_err_msg.format(
                 cell=cell,
