fa3ca3760bebd416efaf44b06d2a1f9c6038c8e8
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2022-09-09 12:36:11 +0000

    Publish 0.6.8
    SHA256 hashes:
    
    nbclient-0.6.8-py3-none-any.whl: 7cce8b415888539180535953f80ea2385cdbb444944cdeb73ffac1556fdbc228
    
    nbclient-0.6.8.tar.gz: 268fde3457cafe1539e32eb1c6d796bbedb90b9e92bacd3e43d83413734bb0e8

--- nbclient/_version.py
+++ nbclient/_version.py
@@ -1,7 +1,7 @@
 import re
 from typing import List, Union
 
-__version__ = "0.6.7"
+__version__ = "0.6.8"
 
 # Build up version_info tuple for backwards compatibility
 pattern = r'(?P<major>\d+).(?P<minor>\d+).(?P<patch>\d+)(?P<rest>.*)'
