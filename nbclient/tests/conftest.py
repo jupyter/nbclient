5b85fe1a3bb0a83466ae293d153ddca6d1530892
Steven Silvester <steven.silvester@ieee.org>
2023-09-10 15:43:57 -0500

    fix tests

--- nbclient/tests/conftest.py
+++ nbclient/tests/conftest.py
@@ -4,6 +4,3 @@ import os
 # instead of randomly generated file names in outputs.
 # See: https://github.com/ipython/ipykernel/blob/360685c6/ipykernel/compiler.py#L50-L55
 os.environ["IPYKERNEL_CELL_NAME"] = "<IPY-INPUT>"
-
-# Opt in to JUPYTER_PLATFORM_DIRS
-os.environ["JUPYTER_PLATFORM_DIRS"] = "1"
